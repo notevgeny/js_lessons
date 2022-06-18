9ee98a96b94c94f2e3a85f1dd918b03ed1c0afe2
notevgeny <gcause17@gmail.com>
2022-06-18 23:48:34 +0300

    logical operators

---
+++ js/filetwo.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// логические операторы
+
+console.log(NaN || 2 || undefined); // 2
+console.log(NaN && 2 && undefined); // NaN
+console.log(1 && 2 && 3); // 3
+console.log(!1 || 2 || !3); // 2
+console.log(25 || null && !3); // 25
+console.log(NaN || null || !3 || undefined); // undefined
+console.log(NaN || null && !3 && undefined); // null
+console.log(5 === 5 && 3 > 1 || 5); // true
+
+
+
+console.log(NaN || null); // null !!
+console.log(null && false); // null !!
+console.log(25 || null); // 25
+
+console.log(5 === 5 && 3 > 1 && 5); // && умножает, || булит 
+
+console.log(NaN || undefined); // !! при && - NaN, при || - undefined
